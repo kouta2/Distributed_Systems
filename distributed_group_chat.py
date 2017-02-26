41a57494450f60501be28e87c4e785a2b0df634f
Kouta <kouta2@sp17-cs425-g32-01.cs.illinois.edu>
2017-02-25 20:11:39 -0600

    add username in send of message

--- distributed_group_chat.py
+++ distributed_group_chat.py
@@ -7,9 +7,19 @@ PORT = 5001
 HOST = ["172.22.146.231", "172.22.146.233", "172.22.146.235", "172.22.146.237", "172.22.146.239"] # all of the hosts allowed in this group ch
 SEND_SOCKS = {} # all of my sockets I need to write to other servers (4 sockets)
 
+DISCONNECTED_CLIENTS = set() # keeps track of the clients who have disconnected
+
 GET_SOCKET = socket.socket
 AF_INET = socket.AF_INET
 SOCK_STREAM = socket.SOCK_STREAM
+
+PROCESS_NUM = int(socket.gethostname()[15:17])
+
+'''
+def handleFailureDetection():
+    for sock in SEND_SOCKS:
+        sock.send(")(*&^%$#@!")
+'''
 
 # handles new connections and messages from other clients
 def handleNewConnections():
@@ -35,26 +45,31 @@ def handleNewConnections():
                     if data[0:4] == "?!@#":
                         CLIENTS[sock] = data[4:]
                         # sys.stdout.write("\r" + CLIENTS[sock] + " entered room" + "\n")
+                    elif sock in DISCONNECTED_CLIENTS:
+                        s = sock
+                        # ignore messages that come from a client after he/she disconnected
                     elif len(data) == 0:
                         sys.stdout.write("\r" + CLIENTS[sock] + " disconnected\n")
                         prompt()
                         del CLIENTS[sock]
+                        DISCONNECTED_CLIENTS.add(sock)
                     else:
-                        sys.stdout.write("\r" + "<" + CLIENTS[sock] + '> ' + data)
+                        sys.stdout.write("\r" + data + '\n')
                         prompt()
                 except:
                     sys.stdout.write("\r" + CLIENTS[sock] + " disconnected\n")
                     prompt()
                     del CLIENTS[sock]
+                    DISCONNECTED_CLIENTS.add(sock)
 
 def prompt():
     sys.stdout.write('<' + username + '> ')
     sys.stdout.flush()
 
-def send_message(msg):
+def send_message(username, msg):
     for s in SEND_SOCKS:
         try:
-            s.send(msg)
+            s.send('<' + username + '> ' + msg)
         except:
             sock = s
             # sys.stdout.write("client message did not get sent\n")
@@ -101,7 +116,7 @@ if __name__=="__main__":
             if sock == sys.stdin:
                 msg = sys.stdin.readline()
                 if len(msg) > 0:
-                    send_message(msg)
+                    send_message(username, msg)
 
     thread_connect.join()
     # thread_fail.join()
