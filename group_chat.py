79a09998becc0109963910e9e9e1dc13c86edcf2
Kouta <kouta2@sp17-cs425-g32-01.cs.illinois.edu>
2017-02-26 23:22:32 -0600

    fixed print issues

--- group_chat.py
+++ group_chat.py
@@ -127,7 +127,9 @@ if __name__=="__main__":
             if sock == sys.stdin:
                 msg = sys.stdin.readline()
                 if len(msg) > 1:
-                    print(CURSOR_UP_ONE_LEVEL + ERASE_LINE)
+                    sys.stdout.write(CURSOR_UP_ONE_LEVEL + ERASE_LINE)
+                    sys.stdout.flush()
+                    prompt()
                     number_of_multicasts += 1
                     local_messages[number_of_multicasts] = '<' + USERNAME + '> ' + msg
                     send_message(create_process_init_message())
